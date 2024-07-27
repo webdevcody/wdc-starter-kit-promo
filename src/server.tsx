14543f94ca936fc7c1cc0f45b5d170e34c64744f
Cody Seibert <webdevcody@gmail.com>
2024-07-27 19:38:19 -0400

    add rss logs

--- src/server.tsx
+++ src/server.tsx
@@ -23,4 +23,21 @@ app.notFound((c) => {
   return c.html("not found bro");
 });
 
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("Unhandled Rejection at:", promise, "reason:", reason);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+});
+
+setInterval(() => {
+  const usage = process.memoryUsage();
+  console.log({
+    rss: usage.rss,
+    heapTotal: usage.heapTotal,
+    heapUsed: usage.heapUsed,
+  });
+}, 60000);
+
 export default app;
